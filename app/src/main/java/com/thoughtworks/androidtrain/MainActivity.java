af028f731a1b0c05c4f108e5e31654a057579f53
jingyu.liu <993506205@qq.com>
2021-07-06 21:02:17 +0800

    add buttons to button activity and config diff language string values

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.java
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.java
@@ -7,11 +7,12 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     public final String TAG = "MainActivity";
+    public final String TAG_BUTTON = "ButtonActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.i(TAG, "MainActivity created");
+        setContentView(R.layout.activity_buttons);
+        Log.i(TAG_BUTTON, "ButtonActivity created");
     }
 }
