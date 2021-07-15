587b94d1080a8cb682749badf857f76e39e7fe5f
jingyu.liu <993506205@qq.com>
2021-07-15 09:47:13 +0800

    login layout works

--- app/src/main/java/com/thoughtworks/androidlearning/MainActivity.java
+++ app/src/main/java/com/thoughtworks/androidlearning/MainActivity.java
@@ -2,20 +2,30 @@ package com.thoughtworks.androidlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buttons);
-        View button1 = findViewById(R.id.constrain_layout);
+        setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.constrain_layout);
         button1.setOnClickListener(
                 button -> {
                     Intent constraintActivityIntent = new Intent(this, ConstraintActivity.class);
                     startActivity(constraintActivityIntent);
+                }
+        );
+
+        Button button2 = findViewById(R.id.login_layout);
+        button2.setOnClickListener(
+                button -> {
+                    Intent loginActivityIntent = new Intent(this, LoginActivity.class);
+                    startActivity(loginActivityIntent);
                 }
         );
     }
